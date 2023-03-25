8826c34c0e12a21a6d3c116e0df9656adf9afaa0
YHJ <278396977@qq.com>
2023-03-25 13:15:38 +0800

    inint

---
+++ chatgpt996-interfaces/src/main/java/com/chatgpt996/api/Application.java
@@ -0,0 +1,15 @@
+package com.chatgpt996.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author author
+ * @create 2023-03-25-12:59
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ chatgpt996-interfaces/src/test/java/com/chatgpt996/api/ApiTest.java
@@ -0,0 +1,8 @@
+package com.chatgpt996.api;
+
+/**
+ * @author author
+ * @create 2023-03-25-13:14
+ */
+public class ApiTest {
+}

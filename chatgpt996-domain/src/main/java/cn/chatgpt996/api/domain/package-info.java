f30876c8d8ed4adfeb57fd3af2140c52d2c9a75d
YHJ <278396977@qq.com>
2023-03-25 13:29:03 +0800

    inint

---
+++ chatgpt996-domain/src/main/java/cn/chatgpt996/api/domain/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author author
+ * @create 2023-03-25-13:27
+ */
+package cn.chatgpt996.api.domain;
---
+++ chatgpt996-infrastructure/src/main/java/cn/chatgpt996/api/infrastructure/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author author
+ * @create 2023-03-25-13:28
+ */
+package cn.chatgpt996.api.infrastructure;
